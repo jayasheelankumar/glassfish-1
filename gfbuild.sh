bb4567166927ad6ccf38ae9f602ad28a151a5802
Romain Grecourt <romain.grecourt@oracle.com>
2018-11-29 20:16:08 -0800

    pass javax.net.debug to JAVA_TOOL_OPTIONS

--- gfbuild.sh
+++ gfbuild.sh
@@ -63,9 +63,8 @@ archive_bundles(){
 }
 
 dev_build(){
-  env
-  cat ${HOME}/.m2/settings.xml
   rm -rf ${HOME}/.m2/repository/org/glassfish/jsonp-jaxrs
+  export JAVA_TOOL_OPTIONS "-Xmx2G -Djavax.net.debug=all"
   export MAVEN_OPTS="-Djavax.net.debug=all"
   mvn -U clean install -Dmaven.test.failure.ignore=true -Pstaging -e
 }
